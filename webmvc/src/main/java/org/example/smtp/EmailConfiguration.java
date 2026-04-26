ed3af44ab12c8615bb1d93316bb099107e856ac3
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2026-04-26 21:09:25 +0300

    ResetPass

--- webmvc/src/main/java/org/example/smtp/EmailConfiguration.java
+++ webmvc/src/main/java/org/example/smtp/EmailConfiguration.java
@@ -2,14 +2,14 @@ package org.example.smtp;
 
 public class EmailConfiguration {
     // Email, з якого відправляємо
-    public static final String FROM = "super.novakvova@ukr.net";
+    public static final String FROM = "dudok405@gmail.com";
     // SMTP-сервер
-    public static final String SMTP_SERVER = "smtp.ukr.net";
+    public static final String SMTP_SERVER = "smtp.gmail.com";
     // Порт
     public static final int PORT = 465;
     // Ім’я користувача
-    public static final String USERNAME = "super.novakvova@ukr.net";
+    public static final String USERNAME = "dudok405@gmail.com";
     // Пароль користувача
-    public static final String PASSWORD = "H4zXcapCSjToppeq";
+    public static final String PASSWORD = "pklh nivd ikpn rfqw";
 }
 
